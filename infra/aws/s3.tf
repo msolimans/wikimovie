02352e997653a5ee31037fc67f375c4ee3b740bd
Muhammad Soliman <m.solimanz@hotmail.com>
2024-04-01 08:49:15 -0400

    fix: topic arn

--- infra/aws/s3.tf
+++ infra/aws/s3.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket_notification" "main" {
   #   count    = var.sns_topic_arn != "" ? 1 : 0
   bucket = aws_s3_bucket.main.id
   topic {
-    topic_arn     = aws_sqs_queue.topic_arn
+    topic_arn     = aws_sqs_queue.main.arn
     events        =   ["s3:ObjectCreated:*"]
     filter_prefix =  "new/"
     filter_suffix = ".json"
